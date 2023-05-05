e3169aae6a678244be6d3694c2ea0a2da308630a
all3yp <noyallepereira@gmail.com>
2023-05-05 19:34:21 -0300

    First commit -m add sun img and simple text

---
+++ WeatherApp/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  WeatherApp
+//
+//  Created by userext on 05/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .center, spacing: 10) {
+            Spacer()
+            HStack(alignment: .center, spacing: 50) {
+                Image(systemName: "sun.max.fill")
+                    .font(.largeTitle)
+                Text("27º")
+                    .font(.largeTitle)
+            }
+            Text("Sunny outside.\nDon't forget your hat!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by userext on 05/05/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
