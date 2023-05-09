11acb16c332751eb69be9d002b0ea8d222051457
all3yp <noyallepereira@gmail.com>
2023-05-08 21:05:32 -0300

    Add watch app target

---
+++ WeatherWatchApp Watch App/WeatherWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherWatchAppApp.swift
+//  WeatherWatchApp Watch App
+//
+//  Created by userext on 08/05/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherWatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
